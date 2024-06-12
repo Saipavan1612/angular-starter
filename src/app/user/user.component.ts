707a3b5309550d7a624b800084bef10bfdcb65fc
Saipavan1612 <saipavangurram@gmail.com>
2024-06-12 11:34:32 +0530

    Register Output using Decorators and output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, input, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output, computed, input, output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,30 +11,34 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  //THIS IS USING InputDecorators
-  @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string;
+  //THIS IS USING Input & Output Decorators
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
 
   //THIS IS USING InputSignals
   // avatar = input.required<string>();
   // name = input.required<string>();
+  //select = output<string>();
 
   // THIS IS USING Signals
   // selectedUser = DUMMY_USERS[randomIndex];
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
 
-   //imagePath = computed(() => 'assets/users/'+this.avatar());
+  //imagePath = computed(() => 'assets/users/'+this.avatar());
 
   get imagePath() {
-    return 'assets/users/'+this.avatar;
+    return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // // this.selectedUser = DUMMY_USERS[randomIndex];
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
     console.log("Clicked!!")
   }
 }
